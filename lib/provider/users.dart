5a04cb853f0a4ccca5130e57a3d6ffe17bd49b06
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-19 00:00:38 +0700

    Add Data: create controller for TextField in provider

--- lib/provider/users.dart
+++ lib/provider/users.dart
@@ -2,5 +2,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Users with ChangeNotifier {
+  TextEditingController firstnameCtrl = TextEditingController();
+  TextEditingController lastnameCtrl = TextEditingController();
+  TextEditingController ageCtrl = TextEditingController();
+
   final db = FirebaseFirestore.instance;
+
+  void addUser() {}
 }
