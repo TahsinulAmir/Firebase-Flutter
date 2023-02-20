a00640d6021404223f55a4add70c43b0e3c7e6d2
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-20 12:18:20 +0700

    Upload Single File: add profider Storage in upload_screen.dart

--- lib/screen/upload_screen.dart
+++ lib/screen/upload_screen.dart
@@ -1,12 +1,16 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
 
 class UploadScreen extends StatelessWidget {
   const UploadScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Storage storage = Provider.of<Storage>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Upload File"),
