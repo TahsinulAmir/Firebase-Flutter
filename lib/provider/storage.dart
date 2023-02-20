8e6ab481decf2677b6706468244c1c655d65f5d0
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-20 22:28:59 +0700

    Upload Single File: add syntax upload file to firebase

--- lib/provider/storage.dart
+++ lib/provider/storage.dart
@@ -1,16 +1,29 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class Storage with ChangeNotifier {
   void UploadFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
 
     if (result != null) {
+      // Mendapatkan file
       File file = File(result.files.single.path!);
+
+      // Upload file ke firebase
+      try {
+        await FirebaseStorage.instance
+            .ref(result.files.single.name)
+            .putFile(file);
+      } on firebase_storage.FirebaseException catch (e) {
+        print(e);
+      }
     } else {
       // User canceled the picker
+      print("Cancel");
     }
   }
 }
