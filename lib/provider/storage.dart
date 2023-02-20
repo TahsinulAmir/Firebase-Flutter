63f5efd4a12be1cf8e22e02fbc451b495b614fbf
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-20 12:16:51 +0700

    Upload Single File: create function UploadFile

--- lib/provider/storage.dart
+++ lib/provider/storage.dart
@@ -1,3 +1,16 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class Storage with ChangeNotifier {}
+class Storage with ChangeNotifier {
+  void UploadFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      File file = File(result.files.single.path!);
+    } else {
+      // User canceled the picker
+    }
+  }
+}
