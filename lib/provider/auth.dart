cfc7378336fed71ff885a2acee6a79a374a31a72
TahsinulAmir <tahsinulamir1@gmail.com>
2023-02-21 13:33:05 +0700

    Authenthication: create changeState

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class Auth with ChangeNotifier {}
+class Auth with ChangeNotifier {
+  // Create authentication
+  var auth = FirebaseAuth.instance;
+
+  // Decalare to save setiap perubahan login/regist
+  Stream<User?> changeState() {
+    return auth.authStateChanges();
+  }
+}
